050e1739f4602278014879a48334a12ad1433dc4
praveen3073 <pop3073@gmail.com>
2020-10-18 22:33:15 +0530

    Added: UC1 BST Create Binary Search Tree

---
+++ main/java/com/hashtableandbst/MyBinaryNode.java
@@ -0,0 +1,13 @@
+package com.hashtableandbst;
+
+public class MyBinaryNode<K> {
+    K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
